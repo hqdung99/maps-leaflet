9bf18ed80fe215cf05515f29cbfcf19c631d30c6
Huynh Quoc Dung <dunghq4@vingroup.net>
2021-11-22 22:09:29 +0700

    resizeable draggable

--- src/App.js
+++ src/App.js
@@ -1,259 +1,346 @@
-import React, { useState, useEffect } from "react";
-import {
-  MapContainer,
-  TileLayer,
-  SVGOverlay,
-  Marker,
-  Popup,
-  Polyline,
-  Circle,
-  CircleMarker,
-  Polygon,
-  Rectangle,
-  LayersControl,
-  ScaleControl,
-  Pane,
-  useMap,
-} from "react-leaflet";
-import {} from "@react-leaflet/core";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import "./leaflet.css";
-import {
-  ThanhPhoHoChiMinh,
-  ThanhPhoHoChiMinh_HuyenCanGio,
-  ThanhPhoHoChiMinh_HuyenNhaBe,
-  ThanhPhoHoChiMinh_HuyenBinhChanh,
-  ThanhPhoHoChiMinh_Quan7,
-  ThanhPhoHoChiMinh_HuyenCuChi,
-  ThanhPhoHoChiMinh_HuyenHocMon,
-  ThanhPhoHoChiMinh_QuanBinhThanh,
-  ThanhPhoHoChiMinh_Quan1,
-  ThanhPhoHoChiMinh_Quan4,
-  ThanhPhoHoChiMinh_Quan8,
-  ThanhPhoHoChiMinh_Quan5,
-  ThanhPhoHoChiMinh_Quan6,
-  ThanhPhoHoChiMinh_Quan11,
-  ThanhPhoHoChiMinh_Quan10,
-  ThanhPhoHoChiMinh_Quan3,
-  ThanhPhoHoChiMinh_QuanPhuNhuan,
-  ThanhPhoHoChiMinh_QuanTanPhu,
-  ThanhPhoHoChiMinh_QuanBinhTan,
-  ThanhPhoHoChiMinh_QuanTanBinh,
-  ThanhPhoHoChiMinh_Quan12,
-  ThanhPhoHoChiMinh_QuanGoVap,
-  ThanhPhoHoChiMinh_ThanhPhoThuDuc,
-} from "./boundary/district";
+import React, { useEffect, useRef } from "react";
+import "./styles.css";
 
-const obj = {
-  ThanhPhoHoChiMinh_HuyenCanGio,
-  ThanhPhoHoChiMinh_HuyenNhaBe,
-  ThanhPhoHoChiMinh_HuyenBinhChanh,
-  ThanhPhoHoChiMinh_Quan7,
-  ThanhPhoHoChiMinh_HuyenCuChi,
-  ThanhPhoHoChiMinh_HuyenHocMon,
-  ThanhPhoHoChiMinh_QuanBinhThanh,
-  ThanhPhoHoChiMinh_Quan1,
-  ThanhPhoHoChiMinh_Quan4,
-  ThanhPhoHoChiMinh_Quan8,
-  ThanhPhoHoChiMinh_Quan5,
-  ThanhPhoHoChiMinh_Quan6,
-  ThanhPhoHoChiMinh_Quan11,
-  ThanhPhoHoChiMinh_Quan10,
-  ThanhPhoHoChiMinh_Quan3,
-  ThanhPhoHoChiMinh_QuanPhuNhuan,
-  ThanhPhoHoChiMinh_QuanTanPhu,
-  ThanhPhoHoChiMinh_QuanBinhTan,
-  ThanhPhoHoChiMinh_QuanTanBinh,
-  ThanhPhoHoChiMinh_Quan12,
-  ThanhPhoHoChiMinh_QuanGoVap,
-  ThanhPhoHoChiMinh_ThanhPhoThuDuc,
-};
-
-const center = [10.775830881777122, 106.70107014486052];
-
-const purpleOptions = {
-  fillColor: "#FD8D3C",
-  fillOpacity: 0.7,
-  weight: 2,
-  opacity: 1,
-  dashArray: "3",
-  color: "white",
-};
-
-const purpleOptions1 = {
-  fillColor: "#BD0026",
-  fillOpacity: 0.7,
-  weight: 2,
-  opacity: 1,
-  dashArray: "3",
-  color: "white",
-};
-
-var info = L.control();
-
-info.onAdd = function (map) {
-  this._div = L.DomUtil.create("div", "info"); // create a div with a class "info"
-  this.update();
-  return this._div;
-};
-
-// method that we will use to uepdswatee the contsrol based on feature properties passed
-info.update = function (props) {
-  this._div.innerHTML =
-    "<h4>US Population Density</h4>" +
-    (props
-      ? "<b>" +
-        props.name +
-        "</b><br />" +
-        props.density +
-        " people / mi<sup>2</sup>"
-      : "Hover over a state");
-};
-
-function MyComponent() {
-  const map = useMap();
+export default function App() {
+  const refContainer = useRef();
+  const ref = useRef();
+  const refLeft = useRef();
+  const refRight = useRef();
+  const refBottom = useRef();
+  const refTop = useRef();
+  // ----
+  const refLeftTop = useRef();
+  const refTopRight = useRef();
+  const refRightBottom = useRef();
+  const refBottomLeft = useRef();
+  const refDragger = useRef();
 
   useEffect(() => {
-    info.addTo(map);
+    const resizeMeEle = ref.current;
+    const styles = window.getComputedStyle(resizeMeEle);
+    let width = parseInt(styles.width, 10);
+    let height = parseInt(styles.height, 10);
+    let x = 0;
+    let y = 0;
+
+    resizeMeEle.style.top = "50px";
+    resizeMeEle.style.left = "50px";
+
+    // Right ------------
+    const onMouseMoveRightResize = (event) => {
+      const dx = event.clientX - x;
+      x = event.clientX;
+      width = width + dx;
+      if (refContainer.current.offsetWidth >= event.clientX) { 
+        resizeMeEle.style.width = `${width}px`;
+      }
+    };
+
+    const onMouseUpRightResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveRightResize);
+    };
+
+    const onMouseDownRightResize = (event) => {
+      x = event.clientX;
+      resizeMeEle.style.left = styles.left;
+      resizeMeEle.style.right = null;
+      document.addEventListener("mousemove", onMouseMoveRightResize);
+      document.addEventListener("mouseup", onMouseUpRightResize);
+    };
+
+    // Top ------
+    const onMouseMoveTopResize = (event) => {
+      const dy = event.clientY - y;
+      height = height - dy;
+      y = event.clientY;
+      if (refContainer.current.offsetTop <= event.clientY) {
+        resizeMeEle.style.height = `${height}px`;
+      }
+    };
+
+    const onMouseUpTopResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveTopResize);
+    };
+
+    const onMouseDownTopResize = (event) => {
+      y = event.clientY;
+      const styles = window.getComputedStyle(resizeMeEle);
+
+      resizeMeEle.style.bottom = styles.bottom;
+      resizeMeEle.style.top = null;
+      document.addEventListener("mousemove", onMouseMoveTopResize);
+      document.addEventListener("mouseup", onMouseUpTopResize);
+    };
+
+    // Left ------
+    const onMouseMoveLeftResize = (event) => {
+      const dx = event.clientX - x;
+      width = width - dx;
+      x = event.clientX;
+      if (refContainer.current.offsetLeft <= event.clientX) {
+        resizeMeEle.style.width = `${width}px`;
+      }
+    };
+
+    const onMouseUpLeftResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveLeftResize);
+    };
+
+    const onMouseDownLeftResize = (event) => {
+      x = event.clientX;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.right = styles.right;
+      resizeMeEle.style.left = null;
+      document.addEventListener("mousemove", onMouseMoveLeftResize);
+      document.addEventListener("mouseup", onMouseUpLeftResize);
+    };
+
+    // Bottom ------
+    const onMouseMoveBottomResize = (event) => {
+      const dy = event.clientY - y;
+      height = height + dy;
+      y = event.clientY;
+      if (refContainer.current.offsetHeight >= event.clientY) {
+        resizeMeEle.style.height = `${height}px`;
+      }
+    };
+
+    const onMouseUpBottomResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveBottomResize);
+    };
+
+    const onMouseDownBottomResize = (event) => {
+      y = event.clientY;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.top = styles.top;
+      resizeMeEle.style.bottom = null;
+      document.addEventListener("mousemove", onMouseMoveBottomResize);
+      document.addEventListener("mouseup", onMouseUpBottomResize);
+    };
+
+    // ---------------------------------------
+    // Left top ------
+    const onMouseMoveLeftTopResize = (event) => {
+      const dy = event.clientY - y;
+      const dx = event.clientX - x;
+      height = height - dy;
+      width = width - dx;
+      y = event.clientY;
+      x = event.clientX;
+      resizeMeEle.style.height = `${height}px`;
+      resizeMeEle.style.width = `${width}px`;
+    };
+
+    const onMouseUpLeftTopResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveLeftTopResize);
+    };
+
+    const onMouseDownLeftTopResize = (event) => {
+      y = event.clientY;
+      x = event.clientX;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.bottom = styles.bottom;
+      resizeMeEle.style.right = styles.right;
+      resizeMeEle.style.left = null;
+      resizeMeEle.style.top = null;
+      document.addEventListener("mousemove", onMouseMoveLeftTopResize);
+      document.addEventListener("mouseup", onMouseUpLeftTopResize);
+    };
+
+    // Top right ------
+    const onMouseMoveTopRightResize = (event) => {
+      const dy = event.clientY - y;
+      const dx = event.clientX - x;
+      height = height - dy;
+      width = width + dx;
+      y = event.clientY;
+      x = event.clientX;
+      resizeMeEle.style.height = `${height}px`;
+      resizeMeEle.style.width = `${width}px`;
+    };
+
+    const onMouseUpTopRightResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveTopRightResize);
+    };
+
+    const onMouseDownTopRightResize = (event) => {
+      y = event.clientY;
+      x = event.clientX;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.bottom = styles.bottom;
+      resizeMeEle.style.left = styles.left;
+      resizeMeEle.style.right = null;
+      resizeMeEle.style.top = null;
+      document.addEventListener("mousemove", onMouseMoveTopRightResize);
+      document.addEventListener("mouseup", onMouseUpTopRightResize);
+    };
+
+    // Right Bottom ------
+    const onMouseMoveRightBottomResize = (event) => {
+      const dy = event.clientY - y;
+      const dx = event.clientX - x;
+      height = height + dy;
+      width = width + dx;
+      y = event.clientY;
+      x = event.clientX;
+      resizeMeEle.style.height = `${height}px`;
+      resizeMeEle.style.width = `${width}px`;
+    };
+
+    const onMouseUpRightBottomResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveRightBottomResize);
+    };
+
+    const onMouseDownRightBottomResize = (event) => {
+      y = event.clientY;
+      x = event.clientX;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.top = styles.top;
+      resizeMeEle.style.left = styles.left;
+      resizeMeEle.style.right = null;
+      resizeMeEle.style.bottom = null;
+      document.addEventListener("mousemove", onMouseMoveRightBottomResize);
+      document.addEventListener("mouseup", onMouseUpRightBottomResize);
+    };
+
+    // Bottom Left ------
+    const onMouseMoveBottomLeftResize = (event) => {
+      const dy = event.clientY - y;
+      const dx = event.clientX - x;
+      height = height + dy;
+      width = width - dx;
+      y = event.clientY;
+      x = event.clientX;
+      resizeMeEle.style.height = `${height}px`;
+      resizeMeEle.style.width = `${width}px`;
+    };
+
+    const onMouseUpBottomLeftResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveBottomLeftResize);
+    };
+
+    const onMouseDownBottomLeftResize = (event) => {
+      y = event.clientY;
+      x = event.clientX;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.top = styles.top;
+      resizeMeEle.style.right = styles.right;
+      resizeMeEle.style.left = null;
+      resizeMeEle.style.bottom = null;
+      document.addEventListener("mousemove", onMouseMoveBottomLeftResize);
+      document.addEventListener("mouseup", onMouseUpBottomLeftResize);
+    };
+
+    // Dragger -----
+    const onMouseMoveDraggerResize = (event) => {
+      const dy = event.clientY - y;
+      const dx = event.clientX - x;
+      const styles = window.getComputedStyle(resizeMeEle);
+      const newTop = parseInt(styles.top) + dy;
+      const newLeft = parseInt(styles.left) + dx;
+      y = event.clientY;
+      x = event.clientX;
+      resizeMeEle.style.top = `${newTop}px`;
+      resizeMeEle.style.left = `${newLeft}px`;
+    };
+
+    const onMouseUpDraggerResize = (event) => {
+      document.removeEventListener("mousemove", onMouseMoveDraggerResize);
+    };
+
+    const onMouseDownDraggerResize = (event) => {
+      y = event.clientY;
+      x = event.clientX;
+      const styles = window.getComputedStyle(resizeMeEle);
+      resizeMeEle.style.top = styles.top;
+      resizeMeEle.style.left = styles.left;
+      resizeMeEle.style.right = null;
+      resizeMeEle.style.bottom = null;
+      document.addEventListener("mousemove", onMouseMoveDraggerResize);
+      document.addEventListener("mouseup", onMouseUpDraggerResize);
+    };
+
+    const resizersRight = refRight.current;
+    const resizersTop = refTop.current;
+    const resizersLeft = refLeft.current;
+    const resizersBottom = refBottom.current;
+    resizersRight.addEventListener("mousedown", onMouseDownRightResize);
+    resizersTop.addEventListener("mousedown", onMouseDownTopResize);
+    resizersLeft.addEventListener("mousedown", onMouseDownLeftResize);
+    resizersBottom.addEventListener("mousedown", onMouseDownBottomResize);
+    // ---
+    const resizersLeftTop = refLeftTop.current;
+    const resizersTopRight = refTopRight.current;
+    const resizersRightBottom = refRightBottom.current;
+    const resizersBottomLeft = refBottomLeft.current;
+    resizersLeftTop.addEventListener("mousedown", onMouseDownLeftTopResize);
+    resizersTopRight.addEventListener("mousedown", onMouseDownTopRightResize);
+    resizersRightBottom.addEventListener(
+      "mousedown",
+      onMouseDownRightBottomResize
+    );
+    resizersBottomLeft.addEventListener(
+      "mousedown",
+      onMouseDownBottomLeftResize
+    );
+    // ---
+    const dragger = refDragger.current;
+    dragger.addEventListener("mousedown", onMouseDownDraggerResize);
 
     return () => {
-      info.remove(map);
+      resizersRight.removeEventListener("mousedown", onMouseDownRightResize);
+      resizersTop.removeEventListener("mousedown", onMouseDownTopResize);
+      resizersLeft.removeEventListener("mousedown", onMouseDownLeftResize);
+      resizersBottom.removeEventListener("mousedown", onMouseDownBottomResize);
+      //
+      resizersLeftTop.removeEventListener(
+        "mousedown",
+        onMouseDownLeftTopResize
+      );
+      resizersTopRight.removeEventListener(
+        "mousedown",
+        onMouseDownTopRightResize
+      );
+      resizersRightBottom.removeEventListener(
+        "mousedown",
+        onMouseDownRightBottomResize
+      );
+      resizersBottomLeft.removeEventListener(
+        "mousedown",
+        onMouseDownBottomLeftResize
+      );
+      dragger.removeEventListener("mousedown", onMouseDownDraggerResize);
     };
   }, []);
-  return null;
-}
 
-var corner1 = L.latLng(11.804410329076212, 104.17574045005802);
-var corner2 = L.latLng(8.537712860574695, 109.37227561043666);
-var bounds = L.latLngBounds(corner1, corner2);
-
-const boundsBox = [
-  [13.248089847492581, 103.45811990209447],
-  [13.254294274473095, 110.75511773284695],
-  [8.184620443541315, 111.52173388635028],
-  [8.064678908160822, 102.73605364377491],
-];
-
-const boundsBoxoptions = {
-  fillColor: "#C9D2D3",
-  fillOpacity: 0.7,
-  weight: 2,
-  opacity: 1,
-  dashArray: "3",
-  color: "white",
-};
-
-function MyApp() {
-  const [selectDistrict, setSelectDistrict] = useState("");
-  console.log("selectDistrict: ", selectDistrict);
-
-  console.log(selectDistrict);
   return (
-    <>
-      <MapContainer
-        center={center}
-        zoom={10}
-        style={{ width: "100vw", height: "100vh" }}
-        maxBounds={bounds}
-        minZoom={9}
-        maxZoom={20}
-        bounds={bounds}
-      >
-        <TileLayer
-          attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-          url="https://api.maptiler.com/maps/voyager/256/{z}/{x}/{y}.png?key=9WFebKyo4SzKO0H6sXG3"
-        />
-        <MyComponent />
-        <Polygon pathOptions={boundsBoxoptions} positions={boundsBox} />
-
-        {ThanhPhoHoChiMinh.data.map((item1) => {
-          const newPoly = item1.ShapeE3.coordinates[0].map((item) => [
-            item[1],
-            item[0],
-          ]);
-          return (
-            <Polygon
-              pathOptions={purpleOptions}
-              positions={newPoly}
-              eventHandlers={{
-                mouseover: (e) => {
-                  console.log("mouseover");
-                  var layer = e.target;
-
-                  layer.setStyle({
-                    weight: 5,
-                    color: "#666",
-                    dashArray: "",
-                    fillOpacity: 0.7,
-                  });
-                  info.update({ name: "Huynh Quoc Dung", density: "full" });
-                },
-                mouseout: (e) => {
-                  var layer = e.target;
-
-                  layer.setStyle({
-                    fillOpacity: 0.7,
-                    weight: 2,
-                    dashArray: "3",
-                    color: "white",
-                  });
-                  info.update();
-                },
-                click: (e) => {
-                  console.log("value: ", selectDistrict);
-                  setSelectDistrict(item1.UniqueName);
-                },
-              }}
-            />
-          );
-        })}
-
-        {selectDistrict &&
-          obj[selectDistrict].data.map((item1) => {
-            const newPoly = item1.ShapeE3.coordinates[0].map((item) => [
-              item[1],
-              item[0],
-            ]);
-
-            return (
-              <Polygon
-                pathOptions={purpleOptions1}
-                positions={newPoly}
-                eventHandlers={{
-                  mouseover: (e) => {
-                    console.log("mouseover");
-                    var layer = e.target;
-
-                    layer.setStyle({
-                      weight: 5,
-                      color: "#666",
-                      dashArray: "",
-                      fillOpacity: 0.7,
-                    });
-                    info.update({ name: "Huynh Quoc Dung", density: "full" });
-                  },
-                  mouseout: (e) => {
-                    var layer = e.target;
-
-                    layer.setStyle({
-                      fillOpacity: 0.7,
-                      weight: 2,
-                      dashArray: "3",
-                      color: "white",
-                    });
-                    info.update();
-                  },
-                  click: (e) => {
-                    setSelectDistrict("");
-                  },
-                }}
-              />
-            );
-          })}
-      </MapContainer>
-    </>
+    <div
+      id="container"
+      ref={refContainer}
+      style={{
+        borderRadius: "5px",
+        width: "800px",
+        height: "800px",
+        background: "#FFBC97",
+        position: "relative",
+      }}
+    >
+      <div ref={ref} className="resizable">
+        <div
+          ref={refDragger}
+          className="dragger"
+        ></div>
+        <div ref={refTop} className="resizer resizer-t"></div>
+        <div ref={refRight} className="resizer resizer-r"></div>
+        <div ref={refBottom} className="resizer resizer-b"></div>
+        <div ref={refLeft} className="resizer resizer-l"></div>
+        {/* ---------- */}
+        <div ref={refLeftTop} className="resizer-angle resizer-lt"></div>
+        <div ref={refTopRight} className="resizer-angle resizer-tr"></div>
+        <div ref={refRightBottom} className="resizer-angle resizer-rb"></div>
+        <div ref={refBottomLeft} className="resizer-angle resizer-bl"></div>
+        <div className="children"></div>
+      </div>
+    </div>
   );
 }
-
-export default MyApp;
---
+++ src/boundary/raw/a.js
@@ -0,0 +1,90 @@
+const a = [
+  {
+    code: "ThanhPhoHoChiMinh_HuyenCanGio",
+    label: "Huyện Cần Giờ",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_HuyenNhaBe",
+    label: "Huyện Nhà Bè",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_HuyenBinhChanh",
+    label: "Huyện Bình Chánh",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan7",
+    label: "Quận 7",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_HuyenCuChi",
+    label: "Huyện Củ Chi",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_HuyenHocMon",
+    label: "Huyện Hóc Môn",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_QuanBinhThanh",
+    label: "Quận Bình Thạnh",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan1",
+    label: "Quận 1",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan4",
+    label: "Quận 4",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan8",
+    label: "Quận 8",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan5",
+    label: "Quận 5",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan6",
+    label: "Quận 6",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan11",
+    label: "Quận 11",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan10",
+    label: "Quận 10",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan3",
+    label: "Quận 3",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_QuanPhuNhuan",
+    label: "Quận Phú Nhuận",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_QuanTanPhu",
+    label: "Quận Tân Phú",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_QuanBinhTan",
+    label: "Quận Bình Tân",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_QuanTanBinh",
+    label: "Quận Tân Bình",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_Quan12",
+    label: "Quận 12",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_QuanGoVap",
+    label: "Quận Gò Vấp",
+  },
+  {
+    code: "ThanhPhoHoChiMinh_ThanhPhoThuDuc",
+    label: "Thành Phố Thủ Đức",
+  },
+];
